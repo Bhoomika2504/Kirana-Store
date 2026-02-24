4d27b489bba46fa20d4f7f5f90b1be6c338bf075
Bhoomika2504 <bhoomikawandhekar@gmail.com>
2026-02-24 11:17:34 +0530

    Initial commit: Setup React, Vite, and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-primary">
+        Kirana Store Setup Complete! 🛒
+      </h1>
+    </div>
+  )
+}
+
+export default App;
